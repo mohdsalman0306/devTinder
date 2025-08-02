1a5ce76ed17017ed9ae86a2f9ea7846e671866af
Salman <mohd.salman0306@gmail.com>
2025-08-02 17:22:47 +0400

    added some api calls

--- src/app.js
+++ src/app.js
@@ -3,6 +3,18 @@ const app = express();
 // app.use("/", (req, res) => {
 // 	res.send("Hello from the server...");
 // });
+app.get("/user", (req, res) => {
+	res.send({ firstname: "Salman", lastname: "Sheikh" });
+});
+
+app.post("/user", (req, res) => {
+	res.send("User saved successfully in DB");
+});
+
+app.delete("/user", (req, res) => {
+	res.send("User deleted successfully in DB");
+});
+
 app.use("/test", (req, res) => {
 	res.send("Hello from the test server...");
 });
