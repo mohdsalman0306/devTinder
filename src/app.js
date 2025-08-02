120ca0b1c63c4209a0f04b852913f31385044024
Salman <mohd.salman0306@gmail.com>
2025-08-02 17:40:40 +0400

    playing with the route

--- src/app.js
+++ src/app.js
@@ -3,23 +3,8 @@ const app = express();
 // app.use("/", (req, res) => {
 // 	res.send("Hello from the server...");
 // });
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
 	res.send({ firstname: "Salman", lastname: "Sheikh" });
-});
-
-app.post("/user", (req, res) => {
-	res.send("User saved successfully in DB");
-});
-
-app.delete("/user", (req, res) => {
-	res.send("User deleted successfully in DB");
-});
-
-app.use("/test", (req, res) => {
-	res.send("Hello from the test server...");
-});
-app.use("/hello", (req, res) => {
-	res.send("Hello Hello Hello ...");
 });
 app.listen(3030, () => {
 	console.log("Server is running on port 3030");
