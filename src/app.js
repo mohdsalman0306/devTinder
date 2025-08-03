7cc7ae7eaca7cb093570acffb73c459c04d7678a
Salman <mohd.salman0306@gmail.com>
2025-08-03 12:04:32 +0400

    next function in the middle of the route handeler

--- src/app.js
+++ src/app.js
@@ -3,9 +3,24 @@ const app = express();
 // app.use("/", (req, res) => {
 // 	res.send("Hello from the server...");
 // });
-app.get("/user/:userId/:name/:password", (req, res) => {
-	res.send({ firstname: "Salman", lastname: "Sheikh" });
-});
+app.use(
+	"/user", // Route path
+
+	(req, res, next) => {
+		// Route handler 1
+		console.log("Response 1");
+		next(); // middleware
+		// res.send("Response 1 ...");
+	},
+	(req, res, next) => {
+		// Route handler 2
+		console.log("Response 2");
+		res.send("Response 2 ...");
+		// next();
+	}
+);
 app.listen(3030, () => {
 	console.log("Server is running on port 3030");
 });
+
+// this is for route handeler and its cases to put next function in the middle of the route handeler
