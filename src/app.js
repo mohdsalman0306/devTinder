23a48bd242b79a439754dd1b808e51512888bee5
Salman <mohd.salman0306@gmail.com>
2025-08-03 12:05:08 +0400

    route handleler in the array

--- src/app.js
+++ src/app.js
@@ -6,21 +6,23 @@ const app = express();
 app.use(
 	"/user", // Route path
 
-	(req, res, next) => {
-		// Route handler 1
-		console.log("Response 1");
-		next(); // middleware
-		// res.send("Response 1 ...");
-	},
-	(req, res, next) => {
-		// Route handler 2
-		console.log("Response 2");
-		res.send("Response 2 ...");
-		// next();
-	}
+	[
+		(req, res, next) => {
+			// Route handler 1
+			console.log("Response 1");
+			next(); // middleware
+			// res.send("Response 1 ...");
+		},
+		(req, res, next) => {
+			// Route handler 2
+			console.log("Response 2");
+			res.send("Response 2 ...");
+			// next();
+		},
+	]
 );
 app.listen(3030, () => {
 	console.log("Server is running on port 3030");
 });
 
-// this is for route handeler and its cases to put next function in the middle of the route handeler
+// we can add the array of the route handeler and  it will run the all the route handleler in the array
